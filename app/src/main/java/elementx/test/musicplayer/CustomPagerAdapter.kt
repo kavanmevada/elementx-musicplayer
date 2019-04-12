901d8ec2e49ce03c392143f856950c78b112327b
Kavan Mevada <kavanmevada@Kavans-iMac.local>
2019-04-12 19:09:28 +0530

    ViewPager inside BottomSheet

--- app/src/main/java/elementx/test/musicplayer/CustomPagerAdapter.kt
+++ app/src/main/java/elementx/test/musicplayer/CustomPagerAdapter.kt
@@ -1,16 +1,21 @@
 package elementx.test.musicplayer
 
+import android.content.Context
+import android.util.AttributeSet
 import android.view.ViewGroup
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
 import elementx.test.musicplayer.databinding.ListItemTwoTxtImgBinding
+import elementx.test.musicplayer.databinding.NowplayingItemViewBinding
 
 
 class CustomPagerAdapter(val list: MutableList<User>) : PagerAdapter() {
 
     override fun instantiateItem(collection: ViewGroup, position: Int): Any {
-        val binding = ListItemTwoTxtImgBinding.inflate(LayoutInflater.from(collection.context), collection, false)
+        val binding = NowplayingItemViewBinding.inflate(LayoutInflater.from(collection.context), collection, false)
         binding.user = list[position]
         collection.addView(binding.root)
         return binding.root
@@ -25,5 +30,24 @@ class CustomPagerAdapter(val list: MutableList<User>) : PagerAdapter() {
     override fun isViewFromObject(view: View, obj: Any): Boolean {
         return view == obj
     }
+}
 
+class CustomViewPager(context: Context, attributeSet: AttributeSet) : ViewPager(context, attributeSet) {
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        var heightMeasureSpece = heightMeasureSpec
+        try {
+            val wrapHeight = View.MeasureSpec.getMode(heightMeasureSpec) == View.MeasureSpec.AT_MOST
+            if (wrapHeight) {
+                val child = getChildAt(0)
+                if (child != null) {
+                    child.measure(widthMeasureSpec, View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED))
+                    val h = child.measuredHeight
+                    heightMeasureSpece = View.MeasureSpec.makeMeasureSpec(h, View.MeasureSpec.EXACTLY)
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpece)
+    }
 }
--- app/src/main/java/elementx/test/musicplayer/MainActivity.kt
+++ app/src/main/java/elementx/test/musicplayer/MainActivity.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomsheet.BottomSheetBehavior
+import elementx.bottomsheet.BottomSheetBehavior
+import elementx.bottomsheet.BottomSheetLayout
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main_content.*
 import kotlinx.android.synthetic.main.bottom_sheet_player.*
 import kotlinx.android.synthetic.main.bottom_sheet_player.view.*
-import androidx.viewpager.widget.ViewPager
-import kotlinx.android.synthetic.main.activity_main_content.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -32,44 +33,43 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        // bottom sheet player behavior
-        BottomSheetBehavior.from(bottom_sheet).apply {
-            state = BottomSheetBehavior.STATE_COLLAPSED
-            isHideable = false
-            imageView.toCardView(16f)
-            setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
-                override fun onStateChanged(bottomSheet: View, newState: Int) {
-                    if (newState == BottomSheetBehavior.STATE_EXPANDED) {
-                        option_panel.visibility = View.VISIBLE
-                    } else option_panel.visibility = View.GONE
+
+        // All bellow these methods are optional
+        // `bottomSheet` is ID of <elementx.bottomsheet.BottomSheetLayout...>
+        // To set it's state before draw
+        bottomSheetLayout.setState(BottomSheetBehavior.STATE_COLLAPSED)
+        bottomSheetLayout.sheet?.imageView?.toCardView(16f)
+        // Add Listen to listen its callback
+        bottomSheetLayout.eventListener = object : BottomSheetLayout.BottomSheetCallback {
+            override fun onStateChanged(bottomSheet: View, newState: BottomSheetBehavior) {
+                if (newState == BottomSheetBehavior.STATE_EXPANDED) {
+                    option_panel.visibility = View.VISIBLE
+                } else option_panel.visibility = View.GONE
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                // it gives animated values from [0-1] during animation
+
+                btn_play_pause.apply {
+                    val offset = slideOffset - 0.5f
+                    if(offset>0) setSheetButton(R.drawable.ic_arrow_down_24px)
+                    else setSheetButton(R.drawable.ic_play_arrow_24px)
                 }
 
-                override fun onSlide(bottomSheet: View, slideOffset: Float) {
-
-                    btn_play_pause.apply {
-                        val offset = slideOffset - 0.5f
-                        if(offset>0) setSheetButton(R.drawable.ic_arrow_down_24px)
-                        else setSheetButton(R.drawable.ic_play_arrow_24px)
-                    }
-
-                    // Convert [0 0.5 1] => [1 0.5 1]
-                    val opacity = slideOffset.let { if (it<.5f) 1f-(it*2f) else (it-.5f)*2f }
-                    btn_play_pause.alpha = opacity
+                // Convert [0 0.5 1] => [1 0.5 1]
+                val opacity = slideOffset.let { if (it<.5f) 1f-(it*2f) else (it-.5f)*2f }
+                btn_play_pause.alpha = opacity
 
 
-                    // Other content to invisible on expand
-                    listOf<View>(
-                            bottomSheet.title,
-                            description,
-                            imageView,
-                            progressBar
-                    ).setAlpha(1f-slideOffset)
-                }
-            })
+                // Other content to invisible on expand
+                listOf<View>(
+                    bottomSheet.title,
+                    description,
+                    imageView,
+                    progressBar
+                ).setAlpha(1f-slideOffset)
+            }
         }
-
-
-        imageView2.toCardView(46f)
 
     }
 
